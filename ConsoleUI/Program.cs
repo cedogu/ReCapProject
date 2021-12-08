f410a1f179982aec6247080b0f1b847cd7b8b665
cedogu <86116050+cedogu@users.noreply.github.com>
2021-12-08 10:54:35 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var c in carManager.GetAll())
+            {
+                Console.WriteLine(c.Description);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
